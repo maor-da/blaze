9cf61c652b46c59e8e2ab2cb5bc49ddd59cfef1f
Maor <77323781+maor-da@users.noreply.github.com>
2023-07-03 21:03:48 +0300

    simple test
    c1
    
    
    c2
    
    
    c3
    
    
    basic version
    
    
    reorder
    
    
    Support all scenarios, next implementation
    
    
    support more types
    
    
    Adding logic and some testing

---
+++ include/blaze.h
@@ -0,0 +1,287 @@
+#include "../glaze/include/glaze/glaze.hpp"
+
+#include <expected>	 // c++23
+#include <map>
+#include <string>
+#include <type_traits>
+#include <variant>
+#include <vector>
+
+namespace json
+{
+
+#define __json_key(x) #x, &T::x
+#define __json_key_offset(x, y) #x, &T::y
+
+#define GET_MACRO(_1, _2, NAME, ...) NAME
+
+// require Yes (/Zc:preprocessor)
+#define json_key(...) GET_MACRO(__VA_ARGS__, __json_key_offset, __json_key)(__VA_ARGS__)
+
+template <int N, typename... T>
+using nth_type = typename std::tuple_element<N, std::tuple<T...>>::type;
+
+template <typename... Args>
+constexpr auto struct_meta(Args&&... args)
+{
+	if constexpr (sizeof...(args) == 0) {
+		return glz::detail::Object{glz::tuplet::tuple{}};
+	} else {
+		return glz::detail::Object{
+			glz::group_builder<std::decay_t<decltype(glz::tuplet::make_copy_tuple(args...))>>::op(
+				glz::tuplet::make_copy_tuple(args...))};
+	}
+}
+
+template <class T, class S, std::size_t N>
+struct key {
+	const char name[N];
+	T S::*offset;
+};
+
+template <class Ty, key... Ts>
+struct json_struct : Ty {
+	struct glaze {
+		using T = Ty;
+		// Thanks to https://stackoverflow.com/a/76596431/1902398
+		static constexpr auto value = std::apply([](auto... args) { return struct_meta(args...); },
+												 std::tuple_cat(std::tuple(Ts.name, Ts.offset)...));
+	};
+};
+
+using primitive_value_t =
+	std::variant<std::string, int64_t, int32_t, uint32_t, uint64_t, bool, double, std::nullptr_t, char>;
+
+using possible_value_t = std::variant<primitive_value_t,
+									  std::vector<int64_t>,
+									  std::vector<int32_t>,
+									  std::vector<uint32_t>,
+									  std::vector<uint64_t>,
+									  std::vector<bool>,
+									  std::vector<double>,
+									  std::vector<char>,
+									  std::vector<std::string>,
+									  std::vector<std::nullptr_t>>;
+
+struct json_doc_t
+	: public std::map<std::string, std::variant<possible_value_t, json_doc_t, std::vector<json_doc_t>>> {
+};
+
+template <typename T>
+concept local_meta_t = glz::detail::local_meta_t<T>;
+
+template <typename T>
+concept not_local_meta_t = requires { !local_meta_t<T>; };
+
+template <typename T>
+concept dynamic_json = std::same_as<T, json_doc_t>;
+
+// using namespace glz; this is not safe since it could shadow other declerations
+template <typename T>
+class _json_string
+{
+public:
+	using doc_t = T;
+
+	template <glz::opts settings = glz::opts{}>
+	std::string pretty()
+	{
+		std::string s{};
+		glz::write<glz::opt_true<settings, &glz::opts::prettify>>(m_Doc, s);
+		return s;
+	}
+
+	template <glz::opts settings>
+	std::string format()
+	{
+		std::string s{};
+		glz::write<settings>(m_Doc, s);
+		return s;
+	}
+
+	// operator and extractors
+	operator std::string()
+	{
+		return glz::write_json(m_Doc);
+	}
+
+protected:
+	template <typename T>
+	friend class _json_base;
+
+	_json_string(doc_t& doc) : m_Doc(doc)
+	{
+		// set the json doc
+	}
+
+private:
+	doc_t& m_Doc;
+};
+
+template <class T>
+class seekable_value
+{
+public:
+	using doc_t = T;
+
+	seekable_value(T& doc, std::string_view key) : key(key), doc(doc) {}
+	seekable_value(const seekable_value&) = delete;
+
+	template <class V>
+	inline constexpr void operator=(V&& value)
+	{
+		// glz::set(doc, key, value)
+		bool result{};
+		glz::detail::seek_impl(
+			[&](auto&& val) {
+				if constexpr (std::is_assignable_v<decltype(val), decltype(value)> /*&&
+							  glz::detail::non_narrowing_convertable<std::decay_t<decltype(value)>,
+																std::decay_t<decltype(val)>>*/) {
+					result = true;
+#if defined(__GNUC__) || defined(__GNUG__)
+#pragma GCC diagnostic push
+#pragma GCC diagnostic ignored "-Waddress"
+#endif
+					// val = static_cast<decltype(val)>(value);
+					val = value;
+#if defined(__GNUC__) || defined(__GNUG__)
+#pragma GCC diagnostic pop
+#endif
+				}
+			},
+			std::forward<T>(doc),
+			key);
+
+		if (!result) {
+			throw std::runtime_error(__FUNCSIG__ "cannot assign or convert value - could be wrong type");
+		}
+	}
+
+	template <class V>
+	inline constexpr operator V()
+	{
+		// auto ret = glz::get<V>(doc, key);
+
+		V* result{};
+		glz::error_code ec{};
+		glz::detail::seek_impl(
+			[&](auto&& val) {
+				if constexpr (!std::is_same_v<V, std::decay_t<decltype(val)>>) {
+					ec = glz::error_code::get_wrong_type;
+				} else if constexpr (!std::is_lvalue_reference_v<decltype(val)>) {
+					ec = glz::error_code::cannot_be_referenced;
+				} else {
+					result = &val;
+				}
+			},
+			std::forward<T>(doc),
+			key);
+		if (!result) {
+			// return glz::unexpected(glz::parse_error{error_code::get_nonexistent_json_ptr});
+			throw std::runtime_error(__FUNCSIG__ "get_nonexistent_json_ptr");
+		} else if (bool(ec)) {
+			// return glz::unexpected(glz::parse_error{ec});
+			throw std::runtime_error(__FUNCSIG__ "get_wrong_type or cannot_be_referenced");
+		}
+		return *result;
+	}
+
+private:
+	std::string key;
+	doc_t& doc;
+};
+
+template <typename T>
+class _json_base
+{
+public:
+	using doc_t = T;
+
+	// Write to string
+	_NODISCARD _json_string<T> dumps()
+	{
+		return {m_Doc};
+	}
+
+	// Load from string
+	template <glz::opts settings = glz::opts{}>
+	_NODISCARD glz::expected<bool, glz::error_code> loads(std::string_view str)
+	{
+		auto err = glz::read<settings>(m_Doc, str);
+		if (err) {
+			return glz::unexpected(err.ec);
+		}
+
+		return true;
+	}
+
+	// Write to file
+	int dumps(int file)
+	{
+		throw std::runtime_error(__FUNCSIG__ "is not implemented");
+	}
+	// Load from file
+	int loads(int file)
+	{
+		throw std::runtime_error(__FUNCSIG__ "is not implemented");
+	}
+
+	// https://github.com/stephenberry/JSON-Pointer
+	constexpr inline auto operator[](std::string_view key)
+	requires(not std::is_same_v<doc_t, json_doc_t>)
+	{
+		if (key.empty()) {
+			return seekable_value(m_Doc, "");
+		} else if (key[0] == '/') {
+			return seekable_value(m_Doc, key);
+		} else {
+			std::string jptr = "/";
+			return seekable_value(m_Doc, jptr + key.data());
+		}
+	}
+
+protected:
+	doc_t m_Doc = {};  // the only storage for the json document
+};
+
+template <class _type = json_doc_t>
+class json : public _json_base<_type>
+{
+public:
+	using doc_t	 = _type;
+	using base_t = _json_base<doc_t>;
+	doc_t& doc	 = base_t::m_Doc;
+
+	template <key... Ts>
+	requires not_local_meta_t<_type>
+	class meta : public _json_base<json_struct<_type, Ts...>>
+	{
+	public:
+		using base_t	   = _json_base<json_struct<_type, Ts...>>;
+		base_t::doc_t& doc = base_t::m_Doc;
+	};
+};
+
+/**
+ * This is a dynamic version which build the json map in runtime
+ * It will be slower - but produce smaller executable
+ */
+template <>
+class json<json_doc_t> : public _json_base<json_doc_t>
+{
+public:
+	using doc_t = json_doc_t;
+
+	// only for json_doc type
+	json_doc_t::mapped_type& operator[](json_doc_t::key_type&& key)
+	{
+		return m_Doc[key];
+	}
+
+	operator const json_doc_t&()
+	{
+		return m_Doc;
+	}
+};
+
+}  // namespace json
---
+++ test/main.cpp
@@ -0,0 +1,154 @@
+﻿#include "../include/blaze.h"
+
+#include <stdint.h>
+#include <windows.h>
+
+#include <iostream>
+
+void dynamic_test()
+{
+	printf("Testing dynamic json builder:\n");
+	json::json j;
+	json::json j2;
+	json::json_doc_t d1;
+
+	std::vector<int> vec{1, 2, 3, 4, 5};
+	std::vector<json::json_doc_t> vec_docs{d1};
+
+	j2["str"]		= "עברית";
+	j2["int"]		= (uint64_t)30;
+	j2["vec"]		= vec;
+	j2["vec_docs"]	= vec_docs;
+
+	j["str"]		= "value";
+	j["int"]		= (uint64_t)30;
+	j["vec"]		= vec;
+	j["vec2"]		= j2;
+
+	std::string str = j.dumps();
+
+	printf("%s\n", str.c_str());
+
+	printf("\n");
+}
+
+struct meta_s {
+	int a;
+	float b;
+	std::string str;
+};
+
+void meta_test()
+{
+	printf("Testing defining meta on instance:\n");
+	json::json<meta_s>::meta<{"a", &meta_s::a}, {"b", &meta_s::b}, {"str", &meta_s::str}> j;
+	json::json<meta_s>::meta<{"a", &meta_s::a}, {"b", &meta_s::b}, {"str", &meta_s::str}> j2;
+	j.doc.a			= 6;
+	j.doc.b			= 9.5;
+	j.doc.str		= "adfsdfsh";
+
+	std::string str = j.dumps().pretty();
+
+	printf("%s\n", str.c_str());
+	printf("%s\n", j2.dumps().pretty().c_str());
+	printf("\n");
+}
+
+struct nested_static_s {
+	int a;
+	float b;
+	std::string str;
+
+	struct glaze {
+		using T						= nested_static_s;
+		static constexpr auto value = json::struct_meta(json_key(a), json_key(b), json_key(str));
+	};
+};
+
+struct static_s {
+	int a;
+	nested_static_s nested;
+
+	struct glaze {
+		using T = static_s;
+		static constexpr auto value =
+			json::struct_meta("#include", glz::file_include{}, json_key(a), json_key(nested));
+	};
+};
+
+struct static_s2 {
+	int a;
+	char c;
+	std::string str;
+	nested_static_s nested;
+
+	struct glaze {
+		using T						= static_s2;
+		static constexpr auto value = json::struct_meta(
+			"#include", glz::file_include{}, json_key(a), json_key(c), json_key(str), json_key(nested));
+	};
+};
+
+struct static_s3 {
+	int a;
+	// char c;
+	bool b;
+	double d;
+	std::vector<int> v;
+	std::string s;
+	nested_static_s nested;
+
+	struct glaze {
+		using T						= static_s3;
+		static constexpr auto value = json::struct_meta("#include",
+														glz::file_include{},
+														json_key(a),
+														json_key(b),
+														// json_key(c),
+														json_key(d),
+														json_key(v),
+														json_key(s),
+														json_key(nested));
+	};
+};
+
+void static_test()
+{
+	printf("Testing predefined meta struct:\n");
+	json::json<static_s> j;
+	json::json<static_s2> j2;
+	json::json<static_s3> j3;
+
+	float b		   = j["/nested/b"];
+	j["/nested/b"] = 7;	 // float{5.0};
+	b			   = j["/nested/b"];
+
+	j.doc.a		   = 4;
+
+	// glz::set(j.doc, "/a", 5);
+	j["a"]			= 5;
+	int a			= j["a"];
+
+	std::string str = j.dumps().pretty();
+
+	printf("%s\n", j.dumps().pretty().c_str());
+	printf("%s\n", j2.dumps().pretty().c_str());
+	printf("%s\n", j3.dumps().pretty().c_str());
+
+	auto ret = j.loads(R"({"a":134567})");
+	if (ret) {
+		printf("%s\n", j.dumps().pretty().c_str());
+	}
+	printf("\n");
+}
+
+void main()
+{
+	SetConsoleOutputCP(65001);
+
+	dynamic_test();
+	meta_test();
+	static_test();
+
+	getchar();
+}
